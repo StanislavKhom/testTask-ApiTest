28bc88ccae0899df6ec450346595a0cfddbe2e46
StanislavKhom <stanislavkhomych6@gmail.com>
2025-01-23 18:49:30 +0200

    Initial and final commit

---
+++ server/app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+// config
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+
+const user = require("./routes/userRoute");
+const product = require("./routes/productRoute");
+const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
+const routerApiTest = require("./routes/ApiTest");
+app.use("/api/user", user);
+app.use("/api/product", product);
+app.use("/api/order", order);
+app.use("/api/payment", payment);
+app.use("/api/apiTest", routerApiTest);
+
+// deployment
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+  
+} else {
+  app.get("/", (req, res) => {
+    res.send(`Server running on port ${PORT} 🚀`);
+  });
+  
+}
+
+
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// error middleware
+// app.use(errorMiddleware);
+
+module.exports = app;
